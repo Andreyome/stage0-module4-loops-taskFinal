d5c106ac0d6015a0c96c38e8a7ca596b448a2808
Andreyome <andreyomem@gmail.com>
2022-12-22 16:06:30 +0200

    commit 1

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,27 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        if (cathetusLength>0){
+            String space="";
+            String output="";
+            for (int i=0;i<cathetusLength-1;i++){
+                space=space+" ";
+            }
+            int row=1;
+            for (int i=0;i<cathetusLength;i++){
+                output+=space;
+                for(int j=0;j<row;j++){
+                    output+="*";
+                }
+                output+="\n";
+                row=row+1;
+                if(space.length()>0) {
+                    space = space.substring(0, (space.length() - 1));
+                }
+            }
+            System.out.println(output);
+        }
+
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+if (cathetusLength>0){
+    String space="";
+    String output="";
+    for (int i=0;i<cathetusLength-1;i++){
+        space=space+" ";
+    }
+    int row=1;
+    for (int i=0;i<cathetusLength;i++){
+        output+=space;
+        for(int j=0;j<row;j++){
+            output+=(row-j);
+        }
+        for(int j=row;j>1;j--){
+            output+=(row-(j-2));
+        }
+        output+="\n";
+        row=row+1;
+        if(space.length()>0) {
+            space = space.substring(0, (space.length() - 1));
+        }
+    }
+    System.out.println(output);
+}
 
     }
 
